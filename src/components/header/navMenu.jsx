0b839aeefee9dc976a7777ce51560a54c5be4561
Louis Horlaville <louis.horlaville@gmail.com>
2020-11-10 01:02:09 -0500

    animation hover nav done

--- src/components/header/navMenu.jsx
+++ src/components/header/navMenu.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 
 class NavMenu extends Component {
-    state = {  }
+    state = { 
+        over1:false,
+        over2:false,
+    };
+
+    
+
     render() { 
+        
         return ( 
             <div className="navMenuContainer headerItems">
-                <a className ="navElements" id="myWorkNav">//MY WORK</a>
-                <a className ="navElements" id="aboutNav">//ABOUT</a>
-            </div>
-        );
-    }
+                <a className ="navElements" id="myWorkNav" href="#">//MY WORK</a>
+                <a className ="navElements" id="aboutNav" href="#">//ABOUT</a>               
+            </div> 
+        )        
+    };
+    
 }
  
 export default NavMenu;
