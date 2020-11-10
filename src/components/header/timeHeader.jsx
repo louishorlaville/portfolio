0a5650ab68533188ed4f234bf1c069f789ff7652
Louis Horlaville <louis.horlaville@gmail.com>
2020-11-09 23:48:53 -0500

    highlights done but animations are a bit buggy

--- src/components/header/timeHeader.jsx
+++ src/components/header/timeHeader.jsx
@@ -6,8 +6,8 @@ class TimeLocation extends Component {
         return ( 
             <div className="timeContainer headerItems">
                 <div className="timeItem" id="timeClock">17:03:55</div>
-                <div className="timeItem" id="timeLocation">Montréal, Canada</div>
-                <div className="timeItem" id="timeLanguage"><span id="english">English</span> // <span id="french">Français</span></div>
+                <div className="timeItem highlightYellow" id="timeLocation">Montréal, Canada</div>
+                <div className="timeItem" id="timeLanguage"><span className ="highlightPink" id="english ">English</span> // <span id="french">Français</span></div>
             </div>
             
          );
