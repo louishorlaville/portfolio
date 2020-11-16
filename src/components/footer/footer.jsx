4a90d71f227ec7398add8f94a015fd4bdf945ca2
Louis Horlaville <louis.horlaville@gmail.com>
2020-11-15 19:55:42 -0500

    gtg eat - decided to vanish the balls with another mask for the responsive, still working on the animation timers

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,28 +7,26 @@ import Logo from '../header/logo';
 class Footer extends Component {
     state = { 
         windowWidth:window.innerWidth,
-        balls:[]
+        balls:[
+            [0,"ballHere"]
+        ]
      };
 
     handleResize = (e) => {
         this.setState({ windowWidth: window.innerWidth });
     };
-
-
+    
     render() { 
         const { windowWidth } = this.state;
         const { balls } = this.state;
-        
-        // this.updateBallArray(windowWidth);
-
         return ( 
             <div className="footerContainer">
                 <div className="pacmanContainer">
                     <img src={Pacman} alt="pacman" id="pacmanFooter"/>
                 </div>
                 <div className="pacmanBallsContainer">
-                    {this.state.balls.map(id=> (
-                        <img id={id} key={id} src={Ball} alt="PacmanBall"/>
+                    {this.state.balls.map((ball, index)=> (
+                        <img id={ball[0]} className={ball[1]} key={index} src={Ball} alt="PacmanBall"/>
                     ))}
                 </div>
             </div>
@@ -39,7 +37,7 @@ class Footer extends Component {
         let ballsRendered = this.state.windowWidth/29; 
         for(let i=0;ballsRendered>i;i++){
             let ballsArray=this.state.balls.slice(0,Math.floor(ballsRendered)-1);
-            ballsArray[i]=i+1;
+            ballsArray[i]=[i+1,"ballHere"];
             this.setState({balls:ballsArray});
         }
     }
@@ -49,7 +47,6 @@ class Footer extends Component {
         window.addEventListener("load", this.updateBallArray);
         window.addEventListener("resize", this.handleResize);
         window.addEventListener("resize", this.updateBallArray);
-        //window.addEventListener("resize", this.fillBalls);
         
     };
 
