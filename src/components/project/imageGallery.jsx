e7c86e2f334c4f9fa3aba55a1ccb2ba900811ffd
Louis Horlaville <louis.horlaville@gmail.com>
2021-01-15 17:22:56 -0500

    img navigation done

--- src/components/project/imageGallery.jsx
+++ src/components/project/imageGallery.jsx
@@ -3,34 +3,45 @@ import React, { Component } from 'react';
 class ImageGallery extends Component {
     state = { 
         projectId:this.props.projectId,
-        imageId:this.props.imageId
+        imageId:this.props.imageId,
+        images:[1,2,3]
      }
+
+    switchLeftRight=(direction, imageId)=>{
+        let allow = (direction==1)? (imageId+1!==4):(imageId-1!==0);
+        if(allow){
+            this.setState({imageId:(direction==1)?imageId+1:imageId-1});
+        }
+    }
+
+    switchImg=(id)=>{
+        this.setState({imageId:id})
+    }
+
     render() { 
         const {projectId} = this.state;
         const {imageId} = this.state;
         return ( 
             <div className="galleryContainer">
-                <div className="galleryContainerBackground">&#10240;</div>
+                <div className="galleryContainerBackground"  onClick={console.log("okok")}>&#10240;</div>
                 <div className="galleryContentContainer">
                     <div className="galleryExpandedImg">
-                        <div id="galleryNavLeft" className="galleryExpandedNavBox">
+                        <div id="galleryNavLeft" className="galleryExpandedNavBox" onClick={()=>this.switchLeftRight(0, imageId)}>
                             <div id="navLeftSymbol" className="navSymbol">&#10240;</div>
                         </div>
+                        {console.log(imageId)}
                         <img id="galleryExpandedImgContent" src={'../../img/projects/'+projectId+'/'+imageId+'.jpg'} alt=""/>
-                        <div id="galleryNavRight" className="galleryExpandedNavBox">
+                        <div id="galleryNavRight" className="galleryExpandedNavBox" onClick={()=>this.switchLeftRight(1, imageId)}>
                             <div id="navRightSymbol" className="navSymbol">&#10240;</div>
                         </div>
                     </div>
+                    
                     <div className="galleryImgRowContainer">
-                        <div className="galleryImgRowItem">
-                            <img src={'../../img/projects/'+projectId+'/1.jpg'} alt=""/>
+                    {this.state.images.map(imageId=>
+                        <div key={imageId} className="galleryImgRowItem" onClick={()=>this.switchImg(imageId)}>
+                            <img src={'../../img/projects/'+projectId+'/'+imageId+'.jpg'} alt=""/>
                         </div>
-                        <div className="galleryImgRowItem">
-                            <img src={'../../img/projects/'+projectId+'/2.jpg'} alt=""/>
-                        </div>
-                        <div className="galleryImgRowItem">
-                            <img src={'../../img/projects/'+projectId+'/3.jpg'} alt=""/>
-                        </div>
+                    )}
                     </div>
                 </div>
             </div>
